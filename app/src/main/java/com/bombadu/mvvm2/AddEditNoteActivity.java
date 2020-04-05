e1ce98b39b5189e822f1d1ee17f3bf7169b0614f
michael <maydev99@gmail.com>
2020-04-05 16:02:42 -0400

    Complete App

--- app/src/main/java/com/bombadu/mvvm2/AddEditNoteActivity.java
+++ app/src/main/java/com/bombadu/mvvm2/AddEditNoteActivity.java
@@ -12,7 +12,7 @@ import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Toast;
 
-public class AddNoteActivity extends AppCompatActivity {
+public class AddEditNoteActivity extends AppCompatActivity {
 
     public static final String EXTRA_TITLE =
             "com.bombadu.mvvm2.EXTRA_TITLE";
@@ -22,6 +22,9 @@ public class AddNoteActivity extends AppCompatActivity {
 
     public static final String EXTRA_PRIORITY =
             "com.bombadu.mvvm2.EXTRA_PRIORITY";
+
+    public static final String EXTRA_ID =
+            "com.bombadu.mvvm2.EXTRA_ID";
 
     private EditText editTextTitle;
     private EditText editTextDescription;
@@ -41,7 +44,18 @@ public class AddNoteActivity extends AppCompatActivity {
         numberPickerPriority.setMaxValue(10);
 
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
-        setTitle("Add Note");
+
+        Intent intent = getIntent();
+        if (intent.hasExtra(EXTRA_ID)) {
+            setTitle("Edit Note");
+            editTextTitle.setText(intent.getStringExtra(EXTRA_TITLE));
+            editTextDescription.setText(intent.getStringExtra(EXTRA_DESCRIPTION));
+            numberPickerPriority.setValue(intent.getIntExtra(EXTRA_PRIORITY, 1));
+
+        } else {
+            setTitle("Add Note");
+        }
+
 
 
     }
@@ -60,6 +74,11 @@ public class AddNoteActivity extends AppCompatActivity {
         data.putExtra(EXTRA_TITLE, title);
         data.putExtra(EXTRA_DESCRIPTION, description);
         data.putExtra(EXTRA_PRIORITY, priority);
+
+        int id = getIntent().getIntExtra(EXTRA_ID, -1);
+        if (id != -1) {
+            data.putExtra(EXTRA_ID, id);
+        }
 
         setResult(RESULT_OK, data);
         finish();
